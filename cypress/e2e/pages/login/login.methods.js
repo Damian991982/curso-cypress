333a29223b1de9a73cc4b99884f880e72ddb386f
Damian991982 <ing.damian.almada@gmail.com>
2025-10-31 19:25:43 -0300

    Fix report and flakyness

--- cypress/e2e/pages/login/login.methods.js
+++ cypress/e2e/pages/login/login.methods.js
@@ -2,10 +2,10 @@ import { LoginElements } from "./login.elements";
 
 export class LoginMethods {
   static insertUsername(username) {
-    LoginElements.textBoxes.username.type(username);
+    LoginElements.textBoxes.username.invoke("val", username);
   }
   static insertPassword(password) {
-    LoginElements.textBoxes.password.type(password);
+    LoginElements.textBoxes.password.invoke("val", password);
   }
   static clickOnLogin() {
     LoginElements.buttons.login.click();
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,14 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import { LoginMethods } from "./pages/login/login.methods";
+
+describe("template spec", () => {
+  it("passes", () => {
+    const usuario = "random01";
+    const contraseña = "random01";
+    cy.visit("https://www.demoblaze.com/index.html");
+    cy.get('a[data-target="#logInModal"]').click();
+    LoginMethods.login(usuario, contraseña);
+    cy.get("a#nameofuser").should("contain.text", usuario);
+
+    cy.wait(10000);
+  });
+});
