7bb7b77cc5ce6c6e1d5421199bc573d179a41c91
Damian991982 <ing.damian.almada@gmail.com>
2025-11-06 00:12:34 -0300

    Creacion de la pagina Place Order

---
+++ cypress/e2e/pages/place-order/place-order.data.js
@@ -0,0 +1,12 @@
+export class PlaceOrderData {
+  static get testData() {
+    return {
+      name: "Test name",
+      country: "United State",
+      city: "Boston",
+      creditCardNumber: "40128888888881881",
+      month: 12,
+      year: 2025,
+    };
+  }
+}
